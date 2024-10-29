b30e03e4ab608dc16f67dad2f573d1e9c466481c
rlarson-db <rlarson.db@gmail.com>
2024-10-29 18:44:28 +0000

    Test Pull Request

--- pages/4_Concessions.py
+++ pages/4_Concessions.py
@@ -2,7 +2,9 @@ import streamlit as st
 import streamlit.components.v1 as components
 import pandas as pd
 
-st.set_page_config(page_title="Concessions", page_icon=':pizza:', layout="wide")
+st.set_page_config(page_title="Concessions", page_icon=':pizza:'
+                   #, layout="wide"
+                   )
 
 df_concessions = pd.read_csv('static/concessions.csv')
 
